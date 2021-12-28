58792eb8c32503269f55878534262ee133176fec
Rutvik Padhiyar <rutvikpadhiyar000@gmail.com>
2021-12-28 10:03:38 +0530

    Formatted better for better readebility and usability
    saperated code in function so than it can be imported from other files.

--- job_name_scrap.py
+++ job_name_scrap.py
@@ -2,22 +2,34 @@ import requests
 from bs4 import BeautifulSoup
 import json
 
-# URL for scraping
-URL = "https://www.careerguide.com/career-options"
 
-# Create soup
-page = requests.get(URL)
-soup = BeautifulSoup(page.content, "html.parser")
+def main():
+    job_dict = get_jobs_dict()
+    # Commit to Json file
+    with open("jobs.json", "w") as fp:
+        json.dump(job_dict, fp)
 
-# Get all required elements from HTML
-job_catags = soup.find_all("h2", class_="c-font-bold")
-job_list = soup.find_all("ul", class_="c-content-list-1 c-theme c-separator-dot")
 
-# Create Dictionary of {job_catagory: job_name}
-job_dict = {}
-for i in range(39):
-    job_dict[job_catags[i].a.text] = [job.a.text for job in job_list[i]]
+def get_jobs_dict(url=None):
+    if url is None:
+        # URL for scraping
+        url = "https://www.careerguide.com/career-options"
 
-# Commit to Json file
-with open("jobs.json", "w") as fp:
-    json.dump(job_dict, fp)
+    # Create soup
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
+
+    # Get all required elements from HTML
+    job_catags = soup.find_all("h2", class_="c-font-bold")
+    job_list = soup.find_all("ul", class_="c-content-list-1 c-theme c-separator-dot")
+
+    # Create Dictionary of {job_catagory: job_list}
+    job_dict = {}
+    for i in range(39):
+        job_dict[job_catags[i].a.text] = [job.a.text for job in job_list[i]]
+
+    return job_dict
+
+
+if __name__ == "__main__":
+    main()
